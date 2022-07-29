e9bba236e85f2ef76ee6e21d8ecdf5b77d7fabfd
Euclides <eneye280@gmail.com>
2022-07-29 13:49:20 -0500

    Update structure project - Scripts

--- Assets/Deep Infinity/Script/Power Ups/PowerUpEffect.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO Deep Infinity / PowerUpEffect")]
-
-public abstract class PowerUpEffect : ScriptableObject
-{
-    [SerializeField] internal abstract void Apply(GameObject target);
-
-}
